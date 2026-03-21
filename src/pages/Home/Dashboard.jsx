9e4177092a9642faf43ef1ea716cf60b518e2b8b
Bharath D <james@Bharaths-MacBook-Air.local>
2026-03-21 16:00:51 +0530

    fix: use resume.id instead of resume._id

--- src/pages/Home/Dashboard.jsx
+++ src/pages/Home/Dashboard.jsx
@@ -39,44 +39,44 @@ const Dashboard = () => {
 
   return <DashboardLayout>
     <div className="grid grid-cols-1 md:grid-cols-5 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0">
-        <div
-          className="h-[300px] flex flex-col gap-5 items-center justify-center bg-white rounded-lg border border-purple-100 hover:border-purple-300 hover:bg-purple-50/5 cursor-pointer"
-          onClick={() => setOpenCreateModal(true)}
-        >
-          <div className="w-12 h-12 flex items-center justify-center bg-purple-200/60 rounded-2xl">
-            <CirclePlus className="text-xl text-purple-500" />
-          </div>
-
-          <h3 className="font-medium text-gray-800">Add New Resume</h3>
+      <div
+        className="h-[300px] flex flex-col gap-5 items-center justify-center bg-white rounded-lg border border-purple-100 hover:border-purple-300 hover:bg-purple-50/5 cursor-pointer"
+        onClick={() => setOpenCreateModal(true)}
+      >
+        <div className="w-12 h-12 flex items-center justify-center bg-purple-200/60 rounded-2xl">
+          <CirclePlus className="text-xl text-purple-500" />
         </div>
 
-        {allResumes?.map((resume, index) => (
-          <ResumeSummaryCard
-            key={resume?._id || `resume-${index}`}
-            imgUrl={resume?.thumbnailLink || null}
-            title={resume.title}
-            lastUpdated={
-              resume?.updatedAt
-                ? moment(resume.updatedAt).format("Do MMM YYYY")
-                : ""
-            }
-            onSelect={()=>navigate(`/resume/${resume?._id}`)}
-          />
-        ))}
+        <h3 className="font-medium text-gray-800">Add New Resume</h3>
       </div>
 
-       <Modal
-        isOpen={openCreateModal}
-        onClose={handleCloseModal}
-        hideHeader
-      >
-        <div>
-          <CreateResumeForm 
-            onResumeCreated={handleResumeCreated}
-            onClose={handleCloseModal}
-          />
-        </div>
-      </Modal>
+      {allResumes?.map((resume, index) => (
+        <ResumeSummaryCard
+          key={resume?.id || `resume-${index}`}
+          imgUrl={resume?.thumbnailLink || null}
+          title={resume.title}
+          lastUpdated={
+            resume?.updatedAt
+              ? moment(resume.updatedAt).format("Do MMM YYYY")
+              : ""
+          }
+          onSelect={() => navigate(`/resume/${resume?.id}`)}
+        />
+      ))}
+    </div>
+
+    <Modal
+      isOpen={openCreateModal}
+      onClose={handleCloseModal}
+      hideHeader
+    >
+      <div>
+        <CreateResumeForm
+          onResumeCreated={handleResumeCreated}
+          onClose={handleCloseModal}
+        />
+      </div>
+    </Modal>
   </DashboardLayout>;
 };
 
